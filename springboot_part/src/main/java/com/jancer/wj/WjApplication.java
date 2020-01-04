6e6f52bca2bcf980f35ef8c4025c3fc0a0752c12
Jancer <3540365582@qq.com>
2020-01-04 15:25:20 +0800

    前后端登录demo
    搞通了，可以登陆成功跳转

---
+++ springboot_part/src/main/java/com/jancer/wj/WjApplication.java
@@ -0,0 +1,13 @@
+package com.jancer.wj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjApplication.class, args);
+    }
+
+}
