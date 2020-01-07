289bd2fc2bcd751a5153e1a68112f0d3e16ee472
Jancer <3540365582@qq.com>
2020-01-07 15:35:49 +0800

    Revert "法律文件分割，使用runsave()写入数据库：章、条；并返回boolean"
    This reverts commit a6aeb0c86018da384625882d588b2c0e4e62a947.

--- springboot_part/src/main/java/com/jancer/wj/dao/LawDao.java
+++ springboot_part/src/main/java/com/jancer/wj/dao/LawDao.java
@@ -1,9 +1,6 @@
 package com.jancer.wj.dao;
 
 
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class LawDao {
@@ -11,6 +8,7 @@ public class LawDao {
     /*提交法律名字到数据库表laws，返回该法律的id*/
     public int addLaw2Base(String lawName){
         int id ;
+
         double d = Math.random();
         id = (int)(d*100);
 
@@ -31,7 +29,6 @@ public class LawDao {
         }
         return flag;
     }
-
     /*判断此行是条*/
     public boolean isSection(String text){
         boolean flag = false;
@@ -70,7 +67,7 @@ public class LawDao {
     //拆分出章
     public static void law_body_split_by_chapter (String law_body){
 
-        String rgex = "第(.{1,4})章";
+        String rgex = "第(.*?)章";
         System.out.println(getSubUtil(law_body, rgex));
 
     }
@@ -91,52 +88,4 @@ public class LawDao {
     }
 
 
-    public boolean runsave(int law_id, List<XWPFParagraph> paragraphs,LawDao dao) {
-        boolean flag = false;
-        int currentChapter = 0; //当前章
-        String currentChapterTittle = ""; //当前章的标题
-        int currentSection = 0; //当前条
-        String currentSectionText = ""; //当前节的内容
-        int chapter_id = 0,section_id = 0;
-
-        //遍历所有段，如果不是空段，进行处理
-        for (XWPFParagraph paragraph: paragraphs) {
-            if (!paragraph.isEmpty()){
-                //如果是章
-                if (dao.isChapter(paragraph.getText())) {
-                    currentChapter ++; //建立新的章
-                    currentChapterTittle = paragraph.getText(); //当前章名称更新
-                    //提交章的标题和法律id到数据库
-                    chapter_id = dao.save2Base("chapter_insert", currentChapterTittle,law_id);
-                    if (chapter_id <= 0){
-                        //章插入出错
-                        flag = false;
-                        break;
-                    }else
-                        flag = true;
-                        continue; //进行下一个循环
-                }
-                //如果是新的条
-                else if (dao.isSection(paragraph.getText())){
-                    //把上一条的全部内容提交
-                    section_id = dao.save2Base("section_insert", currentSectionText, chapter_id);
-                    if (section_id <= 0){
-                        //节插入出错
-                        flag = false;
-                        break;
-                    }else {
-                        currentSection++; //节号更新
-                        currentSectionText = paragraph.getText(); //清空原条内容，更新为新条内容
-                        flag = true;
-                        continue;
-                    }
-                }
-                //该段落 既不是新的章标题，也不是新的条，对上一条内容进行追加
-                else {
-                    currentSectionText +=  paragraph.getText();
-                }
-            }
-        }
-        return flag;
-    }
 }
